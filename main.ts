7684cb960eccd0383c029af0b670fcc2c332a898
h0zz0 <122646454+h0zz0@users.noreply.github.com>
2023-01-25 10:49:17 +0100

    Aktualisiere pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,51 @@
+function run (y: number, x: number) {
+    if (y < 0) {
+        Blink = 1
+    } else {
+        Blink = 0
+    }
+    if (x == 0) {
+        if (y > 0) {
+            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, y)
+            basic.showArrow(ArrowNames.South)
+        }
+        if (y < 0) {
+            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eBackward, y)
+            basic.showArrow(ArrowNames.North)
+        }
+    } else if (x < 0) {
+        if (y > 0) {
+            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, Math.constrain(y - x, 0, 255))
+            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, Math.constrain(y + x, 0, 255))
+            basic.showArrow(ArrowNames.SouthEast)
+            if (y < 0) {
+                DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eBackward, Math.constrain(y - x, 0, 255))
+                DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eBackward, Math.constrain(y + x, 0, 255))
+                basic.showArrow(ArrowNames.SouthWest)
+            }
+        } else if (x > 0) {
+            if (y > 0) {
+                DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, Math.constrain(y - x, 0, 255))
+                DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, Math.constrain(y + x, 0, 255))
+                basic.showArrow(ArrowNames.NorthEast)
+                if (y < 0) {
+                    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eBackward, Math.constrain(y - x, 0, 255))
+                    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eBackward, Math.constrain(y + x, 0, 255))
+                    basic.showArrow(ArrowNames.NorthWest)
+                }
+            }
+        }
+    }
+}
+function doLineTracking (X: number) {
+    if (X < 155) {
+        run(60, Math.constrain(X - 155, 0, -255))
+    } else if (X > 165) {
+        run(60, Math.constrain(X - 165, 0, 255))
+    } else {
+        run(60, 0)
+    }
+}
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "LEDL") {
         DFRobotMaqueenPlusV2.setIndexColor(0, NeoPixelColors.Orange)
@@ -13,9 +61,11 @@ radio.onReceivedValue(function (name, value) {
     if (name == "F") {
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, Math.map(value, 550, 1023, 10, 255))
         basic.showArrow(ArrowNames.South)
+        Blink = 0
     } else if (name == "B") {
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eBackward, Math.map(value, 0, 500, 255, 10))
         basic.showArrow(ArrowNames.North)
+        Blink = 1
     } else if (name == "L") {
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, Math.map(value, 0, 450, 255, 40))
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 10)
@@ -32,16 +82,28 @@ radio.onReceivedValue(function (name, value) {
         basic.clearScreen()
     }
 })
+let X = 0
 let Index = 0
+let Blink = 0
 DFRobotMaqueenPlusV2.I2CInit()
+huskylens.initI2c()
+let mode = 1
+if (mode == 1) {
+    huskylens.initMode(protocolAlgorithm.ALGORITHM_LINE_TRACKING)
+}
 radio.setGroup(200)
-for (let Index2 = 0; Index2 <= 10; Index2++) {
+for (let Index2 = 0; Index2 <= 5; Index2++) {
     music.playMelody("C D E F G F A C5 ", 2000)
 }
-DFRobotMaqueenPlusV2.showColor(NeoPixelColors.White)
+Blink = 1
+if (input.lightLevel() < 80) {
+    DFRobotMaqueenPlusV2.showColor(NeoPixelColors.White)
+}
 DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
 basic.showIcon(IconNames.Yes)
-basic.showNumber(input.temperature())
+for (let Index = 0; Index <= 2; Index++) {
+    basic.showString("" + input.temperature() + " °C")
+}
 basic.forever(function () {
     let US = 0
     Index = DFRobotMaqueenPlusV2.readUltrasonic(DigitalPin.P13, DigitalPin.P14)
@@ -49,13 +111,28 @@ basic.forever(function () {
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 50)
         DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 5)
     }
+    if (mode == 1) {
+        huskylens.request()
+        if (huskylens.isLearned(1)) {
+            if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultArrow)) {
+                X = huskylens.readeBox(1, Content1.xCenter)
+                doLineTracking(X)
+            }
+        }
+    }
 })
 control.inBackground(function () {
-    control.waitForEvent(12, 1)
-    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eLeftLed, MyEnumSwitch.eOpen)
-    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eRightLed, MyEnumSwitch.eClose)
-    basic.pause(500)
-    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eLeftLed, MyEnumSwitch.eClose)
-    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eRightLed, MyEnumSwitch.eOpen)
-    basic.pause(500)
+    while (true) {
+        if (Blink) {
+            DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eLeftLed, MyEnumSwitch.eOpen)
+            DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eRightLed, MyEnumSwitch.eClose)
+            basic.pause(500)
+            DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eLeftLed, MyEnumSwitch.eClose)
+            DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eRightLed, MyEnumSwitch.eOpen)
+            basic.pause(500)
+        } else {
+            DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eAllLed, MyEnumSwitch.eClose)
+            basic.pause(500)
+        }
+    }
 })
